27b4353b2ca32a6dc9194e90104676386e386aa8
Kevin <kevinbalicot@gmail.com>
2023-05-31 17:14:56 +0200

    feat: Ajout des WebComponents :sparkles:

---
+++ front/js/Components/ProductCard.js
@@ -0,0 +1,39 @@
+class ProductCard extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = this.render();
+    }
+
+    get identifier() {
+        return this.getAttribute('identifier');
+    }
+
+    get image() {
+        return this.getAttribute('image');
+    }
+
+    get alt() {
+        return this.getAttribute('alt');
+    }
+
+    get title() {
+        return this.getAttribute('title');
+    }
+
+    get description() {
+        return this.getAttribute('description');
+    }
+
+    render() {
+        return `
+            <article class="product-card">
+                <a class="product-link" href="/html/product.html?id=${this.identifier}">
+                    <img class="product-image" src="${this.image}" alt="${this.alt}">
+                    <h3 class="product-title">${this.title}</h3>
+                    <p class="product-description">${this.description}</p>
+                </a>
+            </article>
+        `;
+    }
+}
+
+window.customElements.define('product-card', ProductCard);
---
+++ front/js/Components/ProductCatalog.js
@@ -0,0 +1,56 @@
+class ProductCatalog extends HTMLElement {
+
+    get apiUrl() {
+        return this.getAttribute('api-url');
+    }
+
+    connectedCallback() {
+        fetch(this.apiUrl, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                }
+
+                throw new Error('Request failed');
+            })
+            .then(jsonResponse => {
+                if (typeof jsonResponse !== 'object') {
+                    throw new Error('Response is not a valid JSON');
+                }
+
+                this.innerHTML = this.render(jsonResponse);
+            })
+            .catch(error => {
+                console.log('Error:', error.message);
+            });
+    }
+
+    render(products) {
+        return `
+            <section class="items" id="items">
+                ${
+                    products.map(product => {
+                        return `
+                            <product-card 
+                                identifier="${product._id}"
+                                title="${product.name}"
+                                description="${product.description}"
+                                image="${product.imageUrl}"
+                                alt="${product.altTxt}"
+                            >
+                            </product-card>
+                        `;
+                    }).join('')
+                }
+            </section>
+        `;
+    }
+}
+
+window.customElements.define('product-catalog', ProductCatalog);
